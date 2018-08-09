1e0baba9bf9e60483114d1fe85f43e98ba361aae
Daniel Veenstra <dan.veenstra@gridplus.io>
2018-08-09 12:34:56 -0500

    Breaking out docker-events into a standalone package

---
+++ src/index.js
@@ -0,0 +1,61 @@
+import { EventEmitter } from 'events';
+import log from 'llog';
+import { spawn } from 'child_process';
+
+class DockerEventEmitter extends EventEmitter {
+  constructor() {
+    super();
+
+    this.cmd = null;
+    this.data = '';
+
+    this._spawnDockerEvents();
+  }
+
+  _spawnDockerEvents() {
+    log.info('spawning docker events');
+
+    this.cmd = spawn(
+      'script -F | docker events --format \'{{json .}}\'',
+      { shell: true },
+    );
+
+    this.cmd.stdout.setEncoding('utf8');
+
+    this.cmd.on('error', (err) => {
+      log.error(`failed to start command, error: ${err}`);
+      this.emit('error', err);
+    });
+
+    this.cmd.stdout.on('data', data => this._tryParseStdoutData(data));
+  }
+
+
+  _tryParseStdoutData(data) {
+    if (!data) return;
+    /* If there is a newline, assume this is multiple JSON lines,
+    separate them, and try to parse them individually
+    */
+    const strings = data.split(/\n/);
+    if (strings.length > 1) {
+      strings.forEach((str) => {
+        this._tryParseStdoutData(str);
+      });
+    } else {
+      this.data = `${this.data}${data.toString()}`;
+      try {
+        const json = JSON.parse(this.data);
+        this.data = '';
+        const eventType = json.Action;
+
+        this.emit(eventType, json);
+      } catch (err) {
+        log.error(err, data);
+        /* no op */
+      }
+    }
+  }
+}
+
+
+export default new DockerEventEmitter();
